ec53c0d6223f950dff717f40fa3de393a51e71d0
nkubota05 <rai.sin0510@icloud.com>
2021-10-02 16:08:52 +0900

    second user commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :orders
-  has_many :items
+  #has_many :orders
+  #has_many :items
 
   validates :nickname,         presence: true
   validates :password,                         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: ' is invalid. Include both letters and numbers'}
