5e7c8dc64dc16770d003e6ffb3dd7ca92e47adce
nkubota05 <rai.sin0510@icloud.com>
2021-10-11 15:59:07 +0900

    first destroy commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :status_id,        numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_day_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id,  numericality: { other_than: 0, message: "can't be blank" }
   validates :prefecture_id,    numericality: { other_than: 1, message: "can't be blank" }
   validates :price, format: { with: /\A[0-9]+\z/ },
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
