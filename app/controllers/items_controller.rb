53d61028b9ed2029f3e560ba3dde338793b81fe9
nkubota05 <rai.sin0510@icloud.com>
2021-10-11 17:21:11 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @Item.user_id 
+    if current_user.id == @item.user_id 
       @item.destroy
       redirect_to root_path
     else
