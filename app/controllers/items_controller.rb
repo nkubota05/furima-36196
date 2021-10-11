04970128b594d6d31c4d887b8085a780760c9907
nkubota05 <rai.sin0510@icloud.com>
2021-10-11 20:27:00 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id
+    if current_user.id == @item.user_id 
       @item.destroy
       redirect_to root_path
     else
